b3a13099e491126d29519db276ab15649b7fcbf9
Hal Blackburn <hwtb2@cam.ac.uk>
2024-08-05 07:34:00 +0000

    feat: add initial datapath snapshot_read request

---
+++ src/denokv/datapath.py
@@ -0,0 +1,39 @@
+import aiohttp
+from fdb.tuple import pack
+
+from denokv._datapath_pb2 import ReadRange, SnapshotRead, SnapshotReadOutput
+from denokv.auth import DatabaseMetadata, EndpointInfo
+
+
+async def snapshot_read(
+    *,
+    session: aiohttp.ClientSession,
+    meta: DatabaseMetadata,
+    endpoint: EndpointInfo,
+    read: SnapshotRead,
+) -> SnapshotReadOutput:
+    url = endpoint.url.joinpath("snapshot_read")
+    async with session.post(
+        url,
+        data=read.SerializeToString(),
+        headers={
+            "authorization": f"Bearer {meta.token}",
+            "content-type": "application/x-protobuf",
+            "x-denokv-database-id": str(meta.database_id),
+            "x-denokv-version": str(meta.version),
+        },
+    ) as response:
+        response.raise_for_status()
+        assert response.headers.get("content-type") == "application/x-protobuf"
+        return SnapshotReadOutput.FromString(await response.read())
+
+
+def snapshot_read_for_get(key: tuple[int | str, ...]) -> SnapshotRead:
+    return SnapshotRead(
+        ranges=[ReadRange(start=pack(key), end=b"\xff", limit=1, reverse=False)]
+    )
+
+
+read = SnapshotRead(ranges=[ReadRange()])
+
+result = SnapshotReadOutput()

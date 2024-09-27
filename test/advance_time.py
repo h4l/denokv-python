42f7d12928b1aeafba84f8e79afd5aa87e7394f6
Hal Blackburn <hwtb2@cam.ac.uk>
2024-09-27 12:56:55 +0000

    test: support advancing event loop time in tests
    The advance_time() function moves the event loop time forward and runs
    any pending timers. This allows pending sleep() / timeout tasks to run.

---
+++ test/advance_time.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import asyncio
+from dataclasses import dataclass
+from dataclasses import field
+from typing import Callable
+
+
+@dataclass
+class AdvancedTime:
+    """Wrapper for a time() function that allows its time to be moved forward."""
+
+    unadvanced: Callable[[], float]
+    _offset: float = field(default=0)
+
+    def __call__(self) -> float:
+        return self.unadvanced() + self._offset
+
+    @property
+    def offset(self) -> float:
+        return self._offset
+
+    def advance_time(self, offset: float) -> None:
+        if not (offset >= 0):
+            raise ValueError(f"offset must be >= 0: {offset=}")
+        self._offset += offset
+
+    def __repr__(self) -> str:
+        return f"<AdvancedTime of {self.unadvanced!r} by offset={self._offset}>"
+
+
+async def advance_time(seconds: float) -> None:
+    """
+    Fast-forward event loop time, running any tasks/timers that have been created.
+
+    The current event loop's time() method will be wrapped with `AdvancedTime`
+    to offset its time into the future and the loop's tasks will be executed up
+    to the new time.
+
+    Notes
+    -----
+    This does not work in uvloop, as patching loop.time() is not observed by the
+    uvloop c/cython implementation. But it doesn't break it either, time just
+    advances at the normal rate.
+
+    The AdvancedTime wrapper for `loop.time()` is not intended to be removed,
+    because event loops use `time.monotonic()` which isn't directly convertible
+    to wall-clock time, but is guaranteed to only increase. This means it should
+    be OK to jump it forwards, but not to move it back by un-patching time().
+    """
+    if not (seconds >= 0):
+        raise ValueError(f"seconds must be >= 0: {seconds=}")
+    loop = asyncio.get_running_loop()
+    if not isinstance(loop.time, AdvancedTime):
+        loop.time = AdvancedTime(unadvanced=loop.time)  # type: ignore[method-assign]
+
+    end = asyncio.create_task(asyncio.sleep(seconds))
+    # Creating a task is not enough to have it scheduled as a timer yet. It must
+    # be scheduled before advancing the loop's time, otherwise it gets scheduled
+    # after the time has advanced and then sleeps the full amount.
+    await asyncio.sleep(0)
+    loop.time.advance_time(seconds)
+    await end  # should take 0 wall-clock time
---
+++ test/test_advance_time.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import asyncio
+
+import pytest
+
+from test.advance_time import advance_time
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_advance_time_with_sleep() -> None:
+    sleep = asyncio.create_task(asyncio.sleep(3))
+    await advance_time(3)
+    await sleep
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_advance_time_with_timeout() -> None:
+    async def should_timeout() -> bool:
+        try:
+            async with asyncio.timeout(2):
+                await asyncio.sleep(3)
+            return False
+        except asyncio.TimeoutError:
+            return True
+
+    timed_out = asyncio.create_task(should_timeout())
+    await advance_time(2)
+    assert timed_out

9ed103d355f128980c6ed65bb20e2353538117af
Hal Blackburn <hwtb2@cam.ac.uk>
2024-10-13 08:36:45 +0000

    refactor: move make_database_metadata_for_endpoint
    Now in denokv_testing so use it in test_kv.

--- test/denokv_testing.py
+++ test/denokv_testing.py
@@ -32,6 +32,7 @@ from fdb.tuple import pack
 from fdb.tuple import unpack
 from google.protobuf.message import Message
 from typing_extensions import TypeIs
+from yarl import URL
 
 from denokv._datapath_pb2 import AtomicWrite
 from denokv._datapath_pb2 import AtomicWriteOutput
@@ -49,6 +50,7 @@ from denokv._datapath_pb2 import SnapshotReadStatus
 from denokv._datapath_pb2 import ValueEncoding
 from denokv._pycompat.dataclasses import slots_if310
 from denokv._pycompat.protobuf import enum_name
+from denokv.auth import ConsistencyLevel
 from denokv.auth import DatabaseMetadata
 from denokv.auth import EndpointInfo
 from denokv.datapath import AnyKvKey
@@ -680,6 +682,31 @@ def mock_db_api(mock_db: MockKvDb) -> web.Application:
     app.router.add_post("/v2/consistency/strong/atomic_write", atomic_write)
     app.router.add_post("/v3/consistency/strong/atomic_write", atomic_write)
     return app
+
+
+def make_database_metadata_for_endpoint(
+    endpoint_url: URL,
+    endpoint_consistency: ConsistencyLevel = ConsistencyLevel.STRONG,
+    version: Literal[1, 2, 3] = 3,
+    database_id: UUID | None = None,
+    expires_at: datetime | None = None,
+    token: str = "hunter2.123",
+) -> tuple[DatabaseMetadata, EndpointInfo]:
+    if database_id is None:
+        database_id = UUID("00000000-0000-0000-0000-000000000000")
+    if expires_at is None:
+        expires_at = datetime.now() + timedelta(minutes=30)
+
+    endpoint = EndpointInfo(url=endpoint_url, consistency=endpoint_consistency)
+
+    meta = DatabaseMetadata(
+        version=version,
+        database_id=database_id,
+        endpoints=[endpoint],
+        expires_at=expires_at,
+        token=token,
+    )
+    return meta, endpoint
 
 
 def add_entries(
